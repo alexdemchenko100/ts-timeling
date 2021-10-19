b406f2ba58c51324a8475b484d906446128a9349
alexdemchenko <alexdemchenko100@gmail.com>
2021-10-19 11:56:49 +0200

    icons

---
+++ src/assets/icon/check.tsx
@@ -0,0 +1,22 @@
+export default (
+  <svg viewBox="141 23 47 47">
+    <rect
+      fill="currentColor"
+      x="168.55"
+      y="20.72"
+      width="7.14"
+      height="50.47"
+      rx="3.57"
+      transform="matrix(0.8, 0.6, -0.6, 0.8, 62.17, -94.22)"
+    />
+    <rect
+      fill="currentColor"
+      x="149.09"
+      y="44.33"
+      width="7.14"
+      height="25.14"
+      rx="3.57"
+      transform="translate(3.96 123.48) rotate(-44.56)"
+    />
+  </svg>
+);
---
+++ src/assets/icon/rose.tsx
@@ -0,0 +1,53 @@
+export default (
+  <svg viewBox="0 0 16 16">
+    <path
+      fill="none"
+      d="M8 9.5V15.5"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M9.70003 12.2C9.30003 12.6 8.70003 12.8 8.10003 12.6C8.00003 12.1 8.10003 11.4 8.50003 11C8.90003 10.6 9.50003 10.4 10.1 10.6C10.3 11.2 10.2 11.8 9.70003 12.2Z"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M7.49999 12.5C7.89999 12.9 8.09999 13.5 7.89999 14.1C7.39999 14.2 6.69999 14.1 6.29999 13.7C5.89999 13.3 5.69999 12.7 5.89999 12.1C6.49999 11.9 7.09999 12.1 7.49999 12.5Z"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M13.5 2.5V3.6C13.5 6.6 11 9.1 8 9.1C5 9.1 2.5 6.6 2.5 3.6V2.5"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M10.5 4.5H4.5C3.4 4.5 2.5 3.6 2.5 2.5C2.5 1.4 3.4 0.5 4.5 0.5H11.5C12.6 0.5 13.5 1.4 13.5 2.5"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M10.5 6.5C12.1 6.5 13.5 5.2 13.5 3.5"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      fill="none"
+      d="M5.5 2.5H10.5C11.1 2.5 11.5 2.9 11.5 3.5C11.5 4.1 11.1 4.5 10.5 4.5"
+      stroke="currentColor"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+  </svg>
+);
