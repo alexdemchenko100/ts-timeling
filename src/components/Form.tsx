b17c3933535d95844abf58829469e6537876c7ce
alexdemchenko <alexdemchenko100@gmail.com>
2021-10-20 10:00:25 +0200

    Show form on error

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -32,17 +32,15 @@ const Form = () => {
 
     try {
       await fetch("https://app.timeling.com/api/misc/signup", {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        // credentials: "include",
+        method: "POST",
+        mode: "cors",
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
       });
+
       setStatus("success");
 
       //For tracking
@@ -60,7 +58,7 @@ const Form = () => {
         <br />
         <FormattedMessage id="its_about_time" />
       </h1>
-      {!status && (
+      {status !== "success" && (
         <form onSubmit={onSubmit}>
           <div className="row">
             <label className="sr-only" htmlFor="Name">
