a7a880c7376f7396ba1b2af24eeca23ed9209596
alexdemchenko <alexdemchenko100@gmail.com>
2021-11-11 15:17:14 +0100

    add norweigan

---
+++ src/string/nb-no.ts
@@ -0,0 +1,42 @@
+const strings = {
+  a_dating_app_for_dates: "En dating app for dates?",
+  cookies: "Cookies",
+  email: "Epost",
+  female: "Kvinne",
+  headline: "Din neste date venter på deg.",
+  headline_sub:
+    "Hils på Timeling. En kalender-basert dating app for ekte relasjoner.",
+  male: "Mann",
+  name: "Navn",
+  its_about_time: "Tiden er inne.",
+  notify_me: "Legg deg til på vår venteliste",
+  lets_meet_up: "La oss møtes",
+  other: "Annen",
+  join_now: "Bli med!",
+  join_the_waitlist: "Legg deg til på vår venteliste",
+  scroll_dates_title: "Scroll dates, ikke profiler",
+  scroll_dates_content_short:
+    "Timeling er en kalender-basert dating-app som lar deg planlegge og gå på dates. Dette sparer deg tid og gjør at du ikke lager forventinger og blir skuffet. Det hjelper deg å bygge nettverk, møte nye menneskjer og kanskje til og med din livspartner. Så begynn å date og så slutt å swipe!",
+  scroll_dates_content_long:
+    "Timeling er en kalender-basert dating-app som lar deg planlegge og gå på dates. Dette sparer deg tid og gjør at du ikke lager forventinger og blir skuffet. Det hjelper deg å bygge nettverk, møte nye menneskjer og kanskje til og med din livspartner. Så begynn å date og så slutt å swipe!",
+  terms_of_service: "Vilkår",
+  privacy_policy: "Personvern",
+  time_we_met_title: "Det var på tide at vi møtes",
+  time_we_met_content_short:
+    "En ny måte å date online på: Med Timeling, kan du sette opp en date, se hvem som liker den og velge en match. Deretter kan du chatte eller ringe for å se om det er en connection, bekrefte og endelig møtes. Slipp å bruk mer tid på endeløs swiping. Timeling er en dating app med en aktiv deadline.",
+  time_we_met_content_long:
+    "En ny måte å date online på: Med Timeling, kan du sette opp en date, se hvem som liker den og velge en match. Deretter kan du chatte eller ringe for å se om det er en connection, bekrefte og endelig møtes. Slipp å bruk mer tid på endeløs swiping. Timeling er en dating app med en aktiv deadline.",
+  ladies_first_title: "Kvinner leder",
+  ladies_first_content_short:
+    "Who runs the world? Jo, kvinner. Dette er derfor vi lagde et system som lar deg confidensielt dele din erfaring med våre admin, slik at vi kan gjøre Timeling til en positiv dating app - nemlig ingen dårlige dates!",
+  ladies_first_content_long:
+    "Who runs the world? Jo, kvinner. Dette er derfor vi lagde et system som lar deg confidensielt dele din erfaring med våre admin, slik at vi kan gjøre Timeling til en positiv dating app - nemlig ingen dårlige dates!",
+  "404_status": "Ikke funnet",
+  thank_you_submission_recieved:
+    "Takk skal du ha! Innsendingen din er mottatt!",
+  something_went_wrong: "Noe gikk galt 😢",
+  unsubscribe: "avslutte abonnement",
+  _has_been_unsubscribed: "{0} har blitt avmeldt",
+  we_value_your_privacy: "Vi verdsetter ditt privatliv",
+};
+export default strings;
