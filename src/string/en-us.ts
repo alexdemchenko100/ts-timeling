4eef5b439c9794d4da3b70b4f59627ae0a2ab3e7
alexdemchenko <alexdemchenko100@gmail.com>
2021-11-18 12:03:38 +0100

    texts

--- src/string/en-us.ts
+++ src/string/en-us.ts
@@ -8,6 +8,9 @@ const strings = {
   form_line_2: "No time-wasting, just dating.",
   follow_us: "Follow us!",
   cookies: "Cookies",
+  cookies_consent: "By using our website, you agree to our cookie policy.",
+  cookies_text:
+    "We use cookies to help improve your experience on our website. This includes analytics cookies and advertising cookies. For more information, please check our Cookies policy. ",
   coming_soon: "Coming soon",
   email: "Email",
   female: "Female",
@@ -15,8 +18,7 @@ const strings = {
   headline_sub: "Your calendar-based dating app for real life connections.",
   headline_sub2:
     "Sign up and get the exclusive chance to win a spot at our launch party at Sawan, Oslo.",
-  headline_sub2_short:
-    "Sign up and get a chance to join our launch party at Sawan, Oslo.",
+  headline_sub2_short: "Sign up and get the chance to join our launch party. ",
   dont_miss_out: "Don't miss out!",
   male: "Male",
   name: "Name",
@@ -28,21 +30,21 @@ const strings = {
   join_the_waitlist: "Join waitlist",
   scroll_dates_title: "Scroll dates,{0}not profiles",
   scroll_dates_content_short:
-    "Timeling is a meet-up app that lets you set and go on dates. Say goodbye to endless chatting, clashing expectations or ghosting – and start meeting new people, friends and maybe even the love of your life.",
+    "The dating app for, well, dates. Say goodbye to endless chatting and time-wasting.",
   scroll_dates_content_long:
     "Timeling is a calendar-based meet-up app that lets you set and go on dates. This saves you endless chatting, clashing expectations or ghosting – and helps you to actually meet new people, friends and maybe even the love of your life. So, stop swiping and start dating.",
   terms_of_service: "Terms of service",
   privacy_policy: "Privacy policy",
   time_we_met_title: "It's time we met",
   time_we_met_content_short:
-    "Online dating, the Timeling way: Simply create a date, see who likes it and choose a match. Chat or call to test the waters, confirm and meet up. No more wasting time, no more frustration. Simple as that.",
+    "Online dating, the Timeling way: Simply create a date, see who likes it and choose a match. Simple as that.",
   time_we_met_content_long:
     "A new kind of online dating: With Timeling, you can create a date, see who likes it and choose a match. Then you can chat or call to test the waters, confirm and meet up. No more wasting time, no more frustration. It’s a dating app with an active deadline, so to say.",
   ladies_first_title: "Ladies first",
   ladies_first_content_short:
-    "Who runs the world? Exactly. That’s why we’ve developed a system that lets you confidentially share your experience with our admins, so that we can make Timeling a bad-date-free place for you to enjoy.",
+    "We’ve developed a system that eliminates creeps and timewasters. Our user rating filters for authenticity and sincerity.",
   ladies_first_content_long:
-    "Who runs the world? Exactly. That’s why we’ve developed a system that lets you confidentially share your experience with our admins, so that we can make Timeling a bad-date-free place for you to enjoy.",
+    "Who runs the world? Exactly. That’s why we’ve developed a system that eliminates creeps, catfishes, ghosters, timewasters and no-showers. Our user rating filters for authenticity and sincerity – and our algorithm makes sure that the people you meet also match.",
   "404_status": "Not found",
   thank_you_submission_recieved:
     "Thank you! Your submission has been received!",
