0cbc2cceab0370de3c2b05c2b91afd4c2c68ab26
alexdemchenko <alexdemchenko100@gmail.com>
2021-12-28 15:17:40 +0100

    Do not use by default

--- src/routes/Cookies.tsx
+++ src/routes/Cookies.tsx
@@ -1,55 +1,58 @@
 import { useState } from "react";
 import { useIntl } from "react-intl";
 import {
-    CookieHeader,
-    CookieExplanation,
-    CookiePreferences,
-    CookieNecessary,
-    CookieOptional,
-    CookieActions,
+  CookieHeader,
+  CookieExplanation,
+  CookiePreferences,
+  CookieNecessary,
+  CookieOptional,
+  CookieActions,
 } from "../components/cookies";
+import { hasConsent } from "../lib/consent";
 import RouteContainer from "../ui/RouteContainer";
 
 import "./Cookies.scss";
 
 const CookiesRoute = () => {
-    const { formatMessage } = useIntl();
+  const { formatMessage } = useIntl();
 
-    const [cookiesPreferences, setCookiePreferences] = useState(true);
+  const [cookiesPreferences, setCookiePreferences] = useState(
+    hasConsent("ads")
+  );
 
-    const routeTitle = formatMessage({ id: "cookies" });
-    const routeDescription = formatMessage({ id: "cookies_description" });
+  const routeTitle = formatMessage({ id: "cookies" });
+  const routeDescription = formatMessage({ id: "cookies_description" });
 
-    const handleAcceptAllCookies = () => setCookiePreferences(true);
+  const handleAcceptAllCookies = () => setCookiePreferences(true);
 
-    const handleSavePreferences = () => null;
+  const handleSavePreferences = () => null;
 
-    const handleUnacceptAllCookies = () => setCookiePreferences(false);
+  const handleUnacceptAllCookies = () => setCookiePreferences(false);
 
-    return (
-        <RouteContainer
-            title={routeTitle}
-            description={routeDescription}
-            name="cookies"
-            className="cookies"
-        >
-            <CookieHeader title={routeTitle} countryName="Nor" />
-            <div className="content-width-1110 content">
-                <CookieExplanation />
-                <CookiePreferences handleOnClick={handleAcceptAllCookies} />
-                <CookieNecessary />
-                <CookieOptional
-                    handleAcceptAllCookies={handleAcceptAllCookies}
-                    handleUnacceptAllCookies={handleUnacceptAllCookies}
-                    cookiePreferences={cookiesPreferences}
-                />
-                <CookieActions
-                    handleAcceptAllCookies={handleAcceptAllCookies}
-                    handleSavePreferences={handleSavePreferences}
-                />
-            </div>
-        </RouteContainer>
-    );
+  return (
+    <RouteContainer
+      title={routeTitle}
+      description={routeDescription}
+      name="cookies"
+      className="cookies"
+    >
+      <CookieHeader title={routeTitle} countryName="Nor" />
+      <div className="content-width-1110 content">
+        <CookieExplanation />
+        <CookiePreferences handleOnClick={handleAcceptAllCookies} />
+        <CookieNecessary />
+        <CookieOptional
+          handleAcceptAllCookies={handleAcceptAllCookies}
+          handleUnacceptAllCookies={handleUnacceptAllCookies}
+          cookiePreferences={cookiesPreferences}
+        />
+        <CookieActions
+          handleAcceptAllCookies={handleAcceptAllCookies}
+          handleSavePreferences={handleSavePreferences}
+        />
+      </div>
+    </RouteContainer>
+  );
 };
 
 export default CookiesRoute;
