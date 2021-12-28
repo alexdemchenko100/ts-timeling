d9922698b87ef1a6122739bf3cbc3b3e434ba30c
alexdemchenko <alexdemchenko100@gmail.com>
2021-12-28 13:49:57 +0100

    Go to cookies screen

--- src/components/consent/Consent.tsx
+++ src/components/consent/Consent.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import Modal from "../Modal";
-import { getAllConsent, getConsent, hasConsent, setConsent } from "../../lib/consent";
+import {
+  getAllConsent,
+  getConsent,
+  hasConsent,
+  setConsent,
+} from "../../lib/consent";
 import { FormattedMessage } from "react-intl";
 import Link from "../global/Link";
 import classNames from "classnames";
@@ -11,74 +16,74 @@ import ConsentForm from "./Form";
 // const getHasConsent = () => !!window.localStorage.getItem("cookiesConsent");
 
 const Consent = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(!hasConsent());
-    const [consentState, setConsentState] = useState(getConsent());
+  const [modalIsOpen, setModalIsOpen] = useState(!hasConsent());
+  const [consentState, setConsentState] = useState(getConsent());
 
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const onHandleAccept = () => {
-        setModalIsOpen(false);
-        setConsent(getAllConsent());
-    };
-    const onHandleSave = () => {
-        setModalIsOpen(false);
-        setConsent(consentState);
-    };
+  const onHandleAccept = () => {
+    setModalIsOpen(false);
+    setConsent(getAllConsent());
+  };
+  const onHandleSave = () => {
+    setModalIsOpen(false);
+    setConsent(consentState);
+  };
 
-    return (
-        <Modal
-            contentLabel="Consent"
-            onClose={() => {}}
-            isOpen={modalIsOpen}
-            className={classNames("popup-consent", { expanded: isExpanded })}
+  return (
+    <Modal
+      contentLabel="Consent"
+      onClose={() => {}}
+      isOpen={modalIsOpen}
+      className={classNames("popup-consent", { expanded: isExpanded })}
+    >
+      <div className="header">
+        {isExpanded && (
+          <>
+            <h1>
+              <FormattedMessage id="cookie_settings" />
+            </h1>
+            <p>
+              <FormattedMessage id="cookie_settings_text" />
+            </p>
+          </>
+        )}
+        {!isExpanded && (
+          <>
+            <FormattedMessage id="cookies_text" />{" "}
+            <Link href="/cookies">
+              <FormattedMessage id="read_more" />
+            </Link>
+          </>
+        )}
+      </div>
+      {isExpanded && (
+        <ConsentForm
+          consent={consentState}
+          onChange={setConsentState}
+          className="content"
+        />
+      )}
+      <div className="bottom">
+        {!isExpanded && (
+          <Link className="button contained" href="/cookies">
+            <FormattedMessage id="set_preferences" />
+          </Link>
+        )}
+        {!!isExpanded && (
+          <Link className="button contained" onClick={onHandleSave}>
+            <FormattedMessage id="save_preferences" />
+          </Link>
+        )}
+        <button
+          className={classNames("button", { primary: isExpanded })}
+          onClick={onHandleAccept}
         >
-            <div className="header">
-                {isExpanded && (
-                    <>
-                        <h1>
-                            <FormattedMessage id="cookie_settings" />
-                        </h1>
-                        <p>
-                            <FormattedMessage id="cookie_settings_text" />
-                        </p>
-                    </>
-                )}
-                {!isExpanded && (
-                    <>
-                        <FormattedMessage id="cookies_text" />{" "}
-                        <Link href="/terms">
-                            <FormattedMessage id="read_more" />
-                        </Link>
-                    </>
-                )}
-            </div>
-            {isExpanded && (
-                <ConsentForm
-                    consent={consentState}
-                    onChange={setConsentState}
-                    className="content"
-                />
-            )}
-            <div className="bottom">
-                {!isExpanded && (
-                    <Link className="button contained" onClick={() => setIsExpanded(true)}>
-                        <FormattedMessage id="set_preferences" />
-                    </Link>
-                )}
-                {!!isExpanded && (
-                    <Link className="button contained" onClick={onHandleSave}>
-                        <FormattedMessage id="save_preferences" />
-                    </Link>
-                )}
-                <button
-                    className={classNames("button", { primary: isExpanded })}
-                    onClick={onHandleAccept}
-                >
-                    <FormattedMessage id="accept_all" />
-                </button>
-            </div>
-        </Modal>
-    );
+          <FormattedMessage id="accept_all" />
+        </button>
+      </div>
+    </Modal>
+  );
 };
 
 export default Consent;
